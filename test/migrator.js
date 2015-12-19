92c66f8455fd1ee82eaca185ab0f81f25c263b4f
Alexei Lexx <alexei.lexx@gmail.com>
2015-12-19 16:08:21 +0200

    add Migrator#rollback

--- test/migrator.js
+++ test/migrator.js
@@ -9,6 +9,7 @@ describe('Migrator', function() {
       migrator = new Migrator([]);
       var result = migrator.migrate();
 
+      expect(result).to.be.ok();
       expect(typeof result.then).to.be('function');
     });
 
@@ -44,14 +45,57 @@ describe('Migrator', function() {
         })
         .then(done, done);
       });
+
+      it('sets the right position', function(done) {
+        expect(migrator.position).to.be(-1);
+
+        migrator.migrate()
+        .then(function() {
+          expect(migrator.position).to.be(2);
+        })
+        .then(done, done);
+      });
+
+      context('when it is called twice', function() {
+        beforeEach(function(done) {
+          migrator.migrate()
+          .then(done, done);
+        });
+
+        it('is fulfilled', function(done) {
+          migrator.migrate()
+          .then(function() {
+            expect(true).to.be(true);
+          }, function(err) {
+            expect(true).to.be(false);
+          })
+          .then(done, done);
+        });
+
+        it('doesn\'t go through the migrations again', function(done) {
+          migrator.migrate()
+          .then(function() {
+            expect(counter).to.be(3);
+          })
+          .then(done, done);
+        });
+
+        it('doesn\'t change the position', function(done) {
+          expect(migrator.position).to.be(2);
+
+          migrator.migrate()
+          .then(function() {
+            expect(migrator.position).to.be(2);
+          })
+          .then(done, done);
+        });
+      });
     });
 
     context('when one migration in the middle fails', function() {
       var flag;
 
       beforeEach(function() {
-        counter = 0;
-
         var migrations = [
           { up: function() { flag = 'A'; } },
           { up: function() { flag = 'B'; } },
@@ -79,6 +123,139 @@ describe('Migrator', function() {
           expect(true).to.be(false);
         }, function(err) {
           expect(flag).to.be('B');
+        })
+        .then(done, done);
+      });
+
+      it('sets the position of the last successful migration', function(done) {
+        migrator.migrate()
+        .then(function() {
+          expect(true).to.be(false);
+        }, function(err) {
+          expect(migrator.position).to.be(1);
+        })
+        .then(done, done);
+      });
+    });
+  });
+
+  describe('rollback', function() {
+    it('returns a promise', function() {
+      migrator = new Migrator([]);
+      var result = migrator.rollback();
+
+      expect(result).to.be.ok();
+      expect(typeof result.then).to.be('function');
+    });
+
+    context('when no migrations are given', function() {
+      beforeEach(function() {
+        migrator = new Migrator([]);
+      });
+
+      it('is rejected', function(done) {
+        migrator.rollback()
+        .then(function() {
+          expect(true).to.be(false);
+        }, function(err) {
+          expect(err.message).to.be('No migrations to rollback');
+        })
+        .then(done, done);
+      });
+    });
+
+    context('when successful migrations are given', function() {
+      var flag;
+
+      beforeEach(function() {
+        var migrations = [
+          { up: function() { flag = 'A'; }, down: function() { flag = null; } },
+          { up: function() { flag = 'B'; }, down: function() { flag = 'A'; } },
+          { up: function() { flag = 'C'; }, down: function() { flag = 'B'; } },
+        ];
+
+        migrator = new Migrator(migrations);
+      });
+
+      context('but they were not migrated yet', function() {
+        it('is rejected', function(done) {
+          migrator.rollback()
+          .then(function() {
+            expect(true).to.be(false);
+          }, function(err) {
+            expect(err.message).to.be('No migrations to rollback');
+          })
+          .then(done, done);
+        });
+      });
+
+      context('and they all were already migrated', function() {
+        beforeEach(function(done) {
+          migrator.migrate()
+          .then(done, done);
+        });
+
+        it('cancels the result of the last one', function(done) {
+          migrator.rollback()
+          .then(function() {
+            expect(flag).to.be('B');
+          })
+          .then(done, done);
+        });
+
+        it('decrements the position', function(done) {
+          expect(migrator.position).to.be(2);
+
+          migrator.rollback()
+          .then(function() {
+            expect(migrator.position).to.be(1);
+          })
+          .then(done, done);
+        });
+      });
+    });
+
+    context('when the last migration fails on rollback', function() {
+      var flag;
+
+      beforeEach(function(done) {
+        var migrations = [
+          { up: function() { flag = 'A'; }, down: function() { flag = null; } },
+          { up: function() { flag = 'B'; }, down: function() { throw new Error(); } },
+        ];
+
+        migrator = new Migrator(migrations);
+        migrator.migrate().then(done, done);
+      });
+
+      it('is rejected', function(done) {
+        migrator.rollback()
+        .then(function() {
+          expect(true).to.be(false);
+        }, function(err) {
+          expect(true).to.be(true);
+        })
+        .then(done, done);
+      });
+
+      it('doesn\'t revert the changes of all migrations', function(done) {
+        migrator.rollback()
+        .then(function() {
+          expect(true).to.be(false);
+        }, function(err) {
+          expect(flag).to.be('B');
+        })
+        .then(done, done);
+      });
+
+      it('doesn\'t decrement the position', function(done) {
+        expect(migrator.position).to.be(1);
+
+        migrator.rollback()
+        .then(function() {
+          expect(true).to.be(false);
+        }, function(err) {
+          expect(migrator.position).to.be(1);
         })
         .then(done, done);
       });
